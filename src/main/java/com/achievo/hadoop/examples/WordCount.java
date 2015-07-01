58eb1cebc3f51cdf8aad2f53c65d6a9cb9e87e1d
bys-galen-zhang <galen.zhang@beyondsoft.com>
2015-07-01 11:16:47 +0800

    Add init commit.

---
+++ src/main/java/com/achievo/hadoop/examples/WordCount.java
@@ -0,0 +1,105 @@
+package com.achievo.hadoop.examples;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+/**
+ * <pre>
+ * 
+ *  Accela Automation
+ *  File: WordCount.java
+ * 
+ *  Accela, Inc.
+ *  Copyright (C): 2015
+ * 
+ *  Description:
+ *  TODO
+ * 
+ *  Notes:
+ * 	$Id: WordCount.java 72642 2009-01-01 20:01:57Z ACHIEVO\galen.zhang $ 
+ * 
+ *  Revision History
+ *  &lt;Date&gt;,			&lt;Who&gt;,			&lt;What&gt;
+ *  Jul 1, 2015		galen.zhang		Initial.
+ * 
+ * </pre>
+ */
+public class WordCount
+{
+	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>
+	{
+		private final static IntWritable one = new IntWritable(1);
+
+		private Text word = new Text();
+
+		@Override
+		protected void map(Object key, Text value, Mapper<Object, Text, Text, IntWritable>.Context context)
+				throws IOException, InterruptedException
+		{
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while (itr.hasMoreTokens())
+			{
+				word.set(itr.nextToken());
+				context.write(word, one);
+			}
+		}
+	}
+
+	public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable>
+	{
+		private IntWritable result = new IntWritable();
+
+		@Override
+		protected void reduce(Text key, Iterable<IntWritable> values,
+				Reducer<Text, IntWritable, Text, IntWritable>.Context context) throws IOException, InterruptedException
+		{
+			int sum = 0;
+			for (IntWritable val : values)
+			{
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+
+	public static void main(String[] args) throws Exception
+	{
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		if (otherArgs.length < 2)
+		{
+			System.err.println("Usage: wordcount <in> [<in>...] <out>");
+			System.exit(2);
+		}
+
+		Job job = Job.getInstance(conf, "word count");
+		job.setJarByClass(WordCount.class);
+		job.setMapperClass(TokenizerMapper.class);
+		job.setCombinerClass(IntSumReducer.class);
+		job.setReducerClass(IntSumReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		for (int i = 0; i < otherArgs.length - 1; ++i)
+		{
+			FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
+		}
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[otherArgs.length - 1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
+
+/*
+ * $Log: av-env.bat,v $
+ */
