910c4ca7e7c160d9a950bb7844624bf7fa7b10bd
bys-galen-zhang <galen.zhang@beyondsoft.com>
2015-07-14 11:21:40 +0800

    Add hbase test case.

--- src/main/java/com/achievo/hadoop/hbase/HBaseTest.java
+++ src/main/java/com/achievo/hadoop/hbase/HBaseTest.java
@@ -1,6 +1,10 @@
 package com.achievo.hadoop.hbase;
 
+import java.util.List;
+
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
@@ -8,6 +12,14 @@ import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Delete;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.Test;
 
 /**
@@ -20,7 +32,7 @@ import org.junit.Test;
  *  Copyright (C): 2015
  * 
  *  Description:
- *  TODO
+ *  http://wuchong.me/blog/2015/04/06/spark-on-hbase-new-api/
  * 
  *  Notes:
  * 	$Id: HBaseTest.java 72642 2009-01-01 20:01:57Z ACHIEVO\galen.zhang $ 
@@ -62,12 +74,70 @@ public class HBaseTest
 		}
 		admin.createTable(tableDescr);
 		System.out.println("Done!");
+		admin.close();
+		connection.close();
+	}
+
+	public static void putRow() throws Exception
+	{
+		Connection connection = ConnectionFactory.createConnection(conf);
+		TableName tableName = TableName.valueOf("user");
+		Table table = connection.getTable(tableName);
+		// 准备插入一条 key 为 1 的数据
+		Put p = new Put("1".getBytes());
+		// 为put操作指定 column 和 value （以前的 put.add 方法被弃用了）
+		p.addColumn("info".getBytes(), "name".getBytes(), "wuchong".getBytes());
+		table.put(p);
+
+		// 查询某条数据
+		Get g = new Get("1".getBytes());
+		Result result = table.get(g);
+		String value = Bytes.toString(result.getValue("info".getBytes(), "name".getBytes()));
+		System.out.println("Get id 1: " + value);
+		outputResult(result);
+
+		// 扫描数据
+		Scan s = new Scan();
+		s.addColumn("info".getBytes(), "name".getBytes());
+		ResultScanner scanner = table.getScanner(s);
+		try
+		{
+			for (Result rs : scanner)
+			{
+				outputResult(rs);
+			}
+		}
+		finally
+		{
+			// 确保scanner关闭
+			scanner.close();
+		}
+
+		// 删除某条数据,操作方式与 Put 类似
+		Delete d = new Delete("1".getBytes());
+		d.addColumn("info".getBytes(), "name".getBytes());
+		table.delete(d);
+
+		table.close();
+		connection.close();
+	}
+
+	public static void outputResult(Result rs)
+	{
+		List<Cell> list = rs.listCells();
+		System.out.println("row key: " + new String(rs.getRow()));
+		for (Cell cell : list)
+		{
+			System.out.println("family: " + new String(CellUtil.cloneFamily(cell)) + ", col: "
+					+ new String(CellUtil.cloneQualifier(cell)) + ", value: " + new String(CellUtil.cloneValue(cell)));
+		}
 	}
 
 	@Test
 	public void testHBase() throws Exception
 	{
 		HBaseTest.createTable();
+		HBaseTest.putRow();
 	}
 }
 
